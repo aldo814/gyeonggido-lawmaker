95c9fb99c32480200ae24eae3347accec2dd116a
aldo814 <66293497+aldo814@users.noreply.github.com>
2022-08-23 14:22:30 +0900

    .

--- html/js/run.js
+++ html/js/run.js
@@ -67,14 +67,14 @@ $(document).ready(function () {
 		autoplay: true,
 		responsive: [ // 반응형 웹 구현 옵션
 			{
-				breakpoint: 1080, //화면 사이즈 960px
+				breakpoint: 1100, 
 				settings: {
 					//위에 옵션이 디폴트 , 여기에 추가하면 그걸로 변경
 					slidesToShow: 2
 				}
 					},
 			{
-				breakpoint: 480, //화면 사이즈 768px
+				breakpoint: 480, 
 				settings: {
 					//위에 옵션이 디폴트 , 여기에 추가하면 그걸로 변경
 					slidesToShow: 1
@@ -292,15 +292,15 @@ $(function () {
 /*snb 메뉴 */
 $(document).ready(function () {
 	$('.snb_drop  .dep1 > a').click(function () {
-        $(this).next('.dep2').slideToggle(100);
-        $(this).parent().toggleClass('on');
-        $(this).parent().siblings('.dep1').removeClass('on');
-        $(this).parent().siblings().children('.dep2:visible').slideUp(0);
-		 $(this).parent().siblings().children('.dep2').children('li').removeClass('active')
+		$(this).next('.dep2').slideToggle(100);
+		$(this).parent().toggleClass('on');
+		$(this).parent().siblings('.dep1').removeClass('on');
+		$(this).parent().siblings().children('.dep2:visible').slideUp(0);
+		$(this).parent().siblings().children('.dep2').children('li').removeClass('active')
 		$(this).parent().siblings().children('.dep2').children('li').removeClass('active')
 
-    });
-	
+	});
+
 	$('.snb_drop  .dep2 >  li > a').click(function () {
 		$(this).parent().toggleClass('active');
 
@@ -347,21 +347,21 @@ $(document).ready(function () {
 				breakpoint: 1250, //화면 사이즈 
 				settings: {
 					//위에 옵션이 디폴트 , 여기에 추가하면 그걸로 변경
-					slidesToShow: 6
+					slidesToShow: 5
 				}
 					},
 			{
 				breakpoint: 960, //화면 사이즈 
 				settings: {
 					//위에 옵션이 디폴트 , 여기에 추가하면 그걸로 변경
-					slidesToShow: 5
+					slidesToShow: 4
 				}
 					},
 			{
 				breakpoint: 800, //화면 사이즈 
 				settings: {
 					//위에 옵션이 디폴트 , 여기에 추가하면 그걸로 변경
-					slidesToShow: 4
+					slidesToShow: 3
 				}
 					},
 			{
@@ -372,7 +372,7 @@ $(document).ready(function () {
 				}
 					},
 			{
-				breakpoint: 480, //화면 사이즈 
+				breakpoint: 620, //화면 사이즈 
 				settings: {
 					//위에 옵션이 디폴트 , 여기에 추가하면 그걸로 변경
 					slidesToShow: 2
@@ -404,109 +404,166 @@ $(document).ready(function () {
 
 		;
 	});
-	
-	
+
+
 	$(document).ready(function () {
-    /*main_visual*/
-    //메인비주얼
-    $('.foreign_main_slick ul').on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
-        var i = (currentSlide ? currentSlide : 0) + 1;
-        $('#slick-log strong').text(i);
-        $('#slick-log span').text(slick.slideCount);
-    });
-    var xpaused = false;
-    var previous = 0;
-    $(".slick-control").on("click", function () {
-        if (xpaused) {
-            $(".main_slick ul").slick("play");
-            $(this).text("정지");
-        } else {
-            $(".main_slick ul").slick("pause");
-            $(this).text("시작");
-        }
-        xpaused = !xpaused;
-        $(this).toggleClass("paused");
-    });
+		/*main_visual*/
+		//메인비주얼
+		$('.foreign_main_slick ul').on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+			var i = (currentSlide ? currentSlide : 0) + 1;
+			$('#slick-log strong').text(i);
+			$('#slick-log span').text(slick.slideCount);
+		});
+		var xpaused = false;
+		var previous = 0;
+		$(".slick-control").on("click", function () {
+			if (xpaused) {
+				$(".foreign_main_slick ul").slick("play");
+				$(this).text("정지");
+			} else {
+				$(".foreign_main_slick ul").slick("pause");
+				$(this).text("시작");
+			}
+			xpaused = !xpaused;
+			$(this).toggleClass("paused");
+		});
 
-    $('.foreign_main_slick  ul').slick({
-        arrow: false,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        prevArrow: $(".fb-prev"),
-        nextArrow: $(".fb-next"),
-        fade:true,
-        autoplaySpeed: 5000,
+		$('.foreign_main_slick  ul').slick({
+			arrow: false,
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			autoplay: true,
+			prevArrow: $(".fb-prev"),
+			nextArrow: $(".fb-next"),
+			fade: true,
+			autoplaySpeed: 5000,
 
 
-    });
-    
-    $('.foreign_main_s2 ul').slick({
-        arrow: true,
-        slidesToShow: 9,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 5000,
-        responsive: [ // 반응형 웹 구현 옵션
-					{  
-						breakpoint: 1024, //화면 사이즈 960px
-						settings: {
-							//위에 옵션이 디폴트 , 여기에 추가하면 그걸로 변경
-							slidesToShow:6 
-						} 
+		});
+
+		$('.foreign_main_s2 ul').slick({
+			arrow: true,
+			slidesToShow: 9,
+			slidesToScroll: 1,
+			autoplay: true,
+			autoplaySpeed: 5000,
+			responsive: [ // 반응형 웹 구현 옵션
+				{
+					breakpoint: 1024, //화면 사이즈 960px
+					settings: {
+						//위에 옵션이 디폴트 , 여기에 추가하면 그걸로 변경
+						slidesToShow: 5
+					}
 					},
-            {  
-						breakpoint: 780, //화면 사이즈 960px
-						settings: {
-							//위에 옵션이 디폴트 , 여기에 추가하면 그걸로 변경
-							slidesToShow:4 
-						} 
+				{
+					breakpoint: 780, //화면 사이즈 960px
+					settings: {
+						//위에 옵션이 디폴트 , 여기에 추가하면 그걸로 변경
+						slidesToShow: 4
+					}
 					},
-					{ 
-						breakpoint: 640, //화면 사이즈 768px
-						settings: {	
-							//위에 옵션이 디폴트 , 여기에 추가하면 그걸로 변경
-							slidesToShow:2 
-						} 
+				{
+					breakpoint: 640, //화면 사이즈 768px
+					settings: {
+						//위에 옵션이 디폴트 , 여기에 추가하면 그걸로 변경
+						slidesToShow: 2
+					}
 					}
 				]
 
 
-    });
-    
-     $('.foreign_member_slider ul').on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
-        var i = (currentSlide ? currentSlide : 0) + 1;
-        $('#slick-log2 strong').text(i);
-        $('#slick-log2 span').text(slick.slideCount);
-    });
-    var xpaused = false;
-    var previous = 0;
-    $(".slick-control2").on("click", function () {
-        if (xpaused) {
-            $(".foreign_member_slider ul").slick("play");
-            $(this).text("정지");
-        } else {
-            $(".foreign_member_slider ul").slick("pause");
-            $(this).text("시작");
-        }
-        xpaused = !xpaused;
-        $(this).toggleClass("paused");
-    });
+		});
 
-    
-      $('.foreign_member_slider ul').slick({
-        arrow: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 5000,
+		$('.foreign_member_slider ul').on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+			var i = (currentSlide ? currentSlide : 0) + 1;
+			$('#slick-log2 strong').text(i);
+			$('#slick-log2 span').text(slick.slideCount);
+		});
+		var xpaused = false;
+		var previous = 0;
+		$(".slick-control2").on("click", function () {
+			if (xpaused) {
+				$(".foreign_member_slider ul").slick("play");
+				$(this).text("정지");
+			} else {
+				$(".foreign_member_slider ul").slick("pause");
+				$(this).text("시작");
+			}
+			xpaused = !xpaused;
+			$(this).toggleClass("paused");
+		});
 
 
-    });
+		$('.foreign_member_slider ul').slick({
+			arrow: true,
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			autoplay: true,
+			autoplaySpeed: 5000,
 
-});
 
-	
+		});
+
+	});
+
+	/*어린의 의회*/
+	/*main_visual*/
+	//메인비주얼
+	$('.child_slider ul').on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+		var i = (currentSlide ? currentSlide : 0) + 1;
+		$('#slick-log strong').text(i);
+		$('#slick-log span').text(slick.slideCount);
+	});
+	var xpaused = false;
+	var previous = 0;
+	$(".slick-control").on("click", function () {
+		if (xpaused) {
+			$(".main_slick").slick("play");
+			$(this).text("정지");
+		} else {
+			$(".main_slick").slick("pause");
+			$(this).text("시작");
+		}
+		xpaused = !xpaused;
+		$(this).toggleClass("paused");
+	});
+
+	$('.child_slider ul').slick({
+		arrow: false,
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		autoplay: true,
+		prevArrow: $(".fb-prev"),
+		nextArrow: $(".fb-next"),
+		autoplaySpeed: 5000,
+        responsive: [ // 반응형 웹 구현 옵션
+				{
+					breakpoint: 780, 
+					settings: {
+						//위에 옵션이 디폴트 , 여기에 추가하면 그걸로 변경
+						slidesToShow: 3
+					}
+					},
+				{
+					breakpoint: 480, 
+					settings: {
+						//위에 옵션이 디폴트 , 여기에 추가하면 그걸로 변경
+						slidesToShow: 2
+					}
+					},
+            {
+					breakpoint: 400, 
+					settings: {
+						//위에 옵션이 디폴트 , 여기에 추가하면 그걸로 변경
+						slidesToShow: 1
+					}
+					}
+				]
+
+
+	});
+
+
 
 
 
@@ -514,10 +571,10 @@ $(document).ready(function () {
 
 	/*파일 업로드 커스텀*/
 	$(document).ready(function (e) {
-		$("input[type='file']").change(function (e) {
+		$(".filebox input[type='file']").change(function (e) {
 
 			//div 내용 비워주기
-			$('#preview').empty();
+			$(this).next().empty();
 
 			var files = e.target.files;
 			var arr = Array.prototype.slice.call(files);
@@ -541,13 +598,13 @@ $(document).ready(function () {
 
 			if (fileSize >= maxSize) {
 				alert('파일 사이즈 초과');
-				$("input[type='file']").val(""); //파일 초기화
+				$(".filebox input[type='file']").val(""); //파일 초기화
 				return false;
 			}
 
 			if (regex.test(fileName)) {
 				alert('업로드 불가능한 파일이 있습니다.');
-				$("input[type='file']").val(""); //파일 초기화
+				$(".filebox input[type='file']").val(""); //파일 초기화
 				return false;
 			}
 			return true;
@@ -561,6 +618,7 @@ $(document).ready(function () {
 				if (fileName.length > 20) {
 					fileName = fileName.substring(0, 20) + "...";
 				}
+                
 
 				//div에 이미지 추가
 				var str = '<div style="display: inline-flex; padding: 5px;"><li>';
@@ -570,7 +628,7 @@ $(document).ready(function () {
 				if (f.type.match('image.*')) {
 					var reader = new FileReader(); //파일을 읽기 위한 FileReader객체 생성
 					reader.onload = function (e) { //파일 읽어들이기를 성공했을때 호출되는 이벤트 핸들러
-						//str += '<button type="button" class="delBtn" value="'+f.name+'" style="background: red">x</button><br>';
+						//str += '<button type="button" class="delBtn" value="'+f.name+'" style="">x</button><br>';
 						str += '<img src="' + e.target.result + '" title="' + f.name + '" width=100 height=100 />';
 						str += '</li></div>';
 						$(str).appendTo('#preview');
@@ -581,11 +639,85 @@ $(document).ready(function () {
 				}
 			}); //arr.forEach
 		}
+       
 	});
 
+/*파일업로드 2개 일때*/
+    $(document).ready(function (e) {
+		$(".filebox2 input[type='file']").change(function (e) {
+
+			//div 내용 비워주기
+			$(this).next().empty();
+
+			var files = e.target.files;
+			var arr = Array.prototype.slice.call(files);
+
+			//업로드 가능 파일인지 체크
+			for (var i = 0; i < files.length; i++) {
+				if (!checkExtension(files[i].name, files[i].size)) {
+					return false;
+				}
+			}
+
+			preview(arr);
 
 
+		}); //file change
 
+		function checkExtension(fileName, fileSize) {
+
+			var regex = new RegExp("(.*?)\.(exe|sh|zip|alz)$");
+			var maxSize = 20971520; //20MB
+
+			if (fileSize >= maxSize) {
+				alert('파일 사이즈 초과');
+				$(".filebox2 input[type='file']").val(""); //파일 초기화
+				return false;
+			}
+
+			if (regex.test(fileName)) {
+				alert('업로드 불가능한 파일이 있습니다.');
+				$(".filebox2 input[type='file']").val(""); //파일 초기화
+				return false;
+			}
+			return true;
+		}
+
+		function preview(arr) {
+			arr.forEach(function (f) {
+
+				//파일명이 길면 파일명...으로 처리
+				var fileName = f.name;
+				if (fileName.length > 20) {
+					fileName = fileName.substring(0, 20) + "...";
+				}
+                
+
+				//div에 이미지 추가
+				var str = '<div style="display: inline-flex; padding: 5px;"><li>';
+				str += '<span>' + fileName + '</span><br>';
+
+				//이미지 파일 미리보기
+				if (f.type.match('image.*')) {
+					var reader = new FileReader(); //파일을 읽기 위한 FileReader객체 생성
+					reader.onload = function (e) { //파일 읽어들이기를 성공했을때 호출되는 이벤트 핸들러
+						//str += '<button type="button" class="delBtn" value="'+f.name+'" style="">x</button><br>';
+						str += '<img src="' + e.target.result + '" title="' + f.name + '" width=100 height=100 />';
+						str += '</li></div>';
+						$(str).appendTo('#preview2');
+					}
+					reader.readAsDataURL(f);
+				} else {
+					$(str).appendTo('#preview2');
+				}
+			}); //arr.forEach
+		}
+	});
+
+/*이미지 삭제 버튼
+     $('.delBtn').click(function(){
+            $(this).parent().remove();
+ });*/
 });
 
 /*아코디언 메뉴*/
@@ -670,5 +802,167 @@ $(document).ready(function () {
 	});
 
 
+	// 상세검색
+	$('.sd_btn').click(function () {
+		$(this).toggleClass('on');
+		
+	if ($(".sd_btn").hasClass("on") === true) {
+
+		// 속성값이 존재함.
+		$(this).html('상세검색 닫기');
+		$('.detail').show();
+		$('.default').hide();
+	} else {
+		
+		$(this).html('상세검색');
+		$('.detail').hide();
+		$('.default').show();
+	}
+	});
+
+    //지역상담소 지도 hover시 
+ $('#image-maps area').mouseover(function(){
+      var showId = $(this).attr('id');
+      $(showId).show();
+ });
+ 
+ $('#image-maps area').mouseout(function(){
+      var hideId = $(this).attr('id');
+      $(hideId).hide();
+ });
+    
+    // 지역상담소 탭
+   	// List Tab Navigation
+	var $tab_list = $('.consulting_tab');
+	$tab_list.removeClass('jx').find('ul ul').hide();
+	$tab_list.find('li li.active').parents('li').addClass('active');
+	$tab_list.find('li.active>ul').show();
+	$tab_list.each(function(){
+		var $this = $(this);
+		$this.height($this.find('li.active>ul').height()+40);
+	});
+	function listTabMenuToggle(event){
+		var $this = $(this);
+		$this.next('ul').show().parent('li').addClass('active').siblings('li').removeClass('active').find('>ul').hide();
+		$this.closest('.consulting_tab').height($this.next('ul').height()+40);
+		if($this.attr('href') === '#'){
+			return false;
+		}
+	}
+	$tab_list.find('>ul>li>a').click(listTabMenuToggle).focus(listTabMenuToggle);
+    
+	//지역상담소 배너
+	$('.consulting_banner ul').on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+		var i = (currentSlide ? currentSlide : 0) + 1;
+		$('.consulting_banner #slick-log strong').text(i);
+		$('.consulting_banner #slick-log span').text(slick.slideCount);
+	});
+	var xpaused = false;
+	var previous = 0;
+	$(".cons-btn .slick-control").on("click", function () {
+		if (xpaused) {
+			$(".consulting_banner ul").slick("play");
+			$(this).text("정지");
+		} else {
+			$(".consulting_banner ul").slick("pause");
+			$(this).text("시작");
+		}
+		xpaused = !xpaused;
+		$(this).toggleClass("paused");
+	});
+
+	$('.consulting_banner ul').slick({
+		arrow: false,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplay: true,
+        dots:true,
+		prevArrow: $(".fb-prev"),
+		nextArrow: $(".fb-next"),
+		autoplaySpeed: 5000,
+
+
+	});
+    
+    /*기자실*/
+    $(".press_main_visual ul").slick({
+		fade:true,
+		infinite: true,
+		autoplay:true,
+		arrows:false,
+		autoplaySpeed: 10000,
+		dots:true,
+		customPaging: function (slider, i) {
+			var title = $(slider.$slides[i]).data('title');
+			return '<span class="dots__item">' + title + ' </span>';
+		},
+		dotsClass: 'slider-dots'
+	});
+    
+    
+    /*모바일*/
+   if($(window).width() < 1025) {
+   $('.snb_wrap .snb_tit').click(function(){
+      $(this).parent().toggleClass('on')
+   });
+   }
+
+
+    /*통합검색 js*/
+    $('.sch_box .tit').click(function(){
+        $(this).parent().toggleClass('on');
+    });
+    
+    $('.rank_tab.weekly').click(function(){
+       $(this).addClass('on'); 
+       $('.rank.weekly').show();
+       $('#dayPopKey').hide();
+       $('.rank_tab.daily').removeClass('on')
+    });
+     $('.rank_tab.daily').click(function(){
+       $(this).addClass('on'); 
+       $('.rank.daily').show();
+       $('#weekPopKey').hide();
+       $('.rank_tab.weekly').removeClass('on') 
+    });
+    
+    
+        //청사안내_상징물
+    $('.Information_thumb .img01').click(function(){
+        $(this).addClass('active');
+        $('.Information_thumb .img02').removeClass('active');
+        $('.view_larger.img01').addClass('active');
+        $('.view_larger.img02').removeClass('active');
+    });
+    $('.Information_thumb .img02').click(function(){
+        $(this).addClass('active');
+        $('.Information_thumb .img01').removeClass('active');
+        $('.view_larger.img02').addClass('active');
+        $('.view_larger.img01').removeClass('active');
+    });
+
+    //개인정보 처리방침
+    $('.privacy_wrap .round_tit h2 a').click(function(){
+       $(this).parent().next().slideToggle(); 
+        $(this).parent().toggleClass('on')
+    });
+
 
 });
+
+$(window).resize(function() { if($(window).width() > 1025) { /*모바일*/
+  $('.snb_wrap .snb_tit').click(function(){
+      $(this).parent().toggleClass('on');
+      
+   });
+   }
+}); 
+
+$(window).resize(function() { if($(window).width() > 390) { /*모바일*/
+ 
+    
+    
+   }
+}); 
+
+
